50df69b84ec36449737f3dfef4ed9fe1628715a0
Walter Lim <waltissomewhere@gmail.com>
2019-04-28 12:21:46 +1200

    gyro fix

--- js/colormagic.js
+++ js/colormagic.js
@@ -10,6 +10,45 @@ var mouseX = 0;
 var mouseY = 0;
 var colorHue;
 var colorSaturation;
+let gyroscope = new Gyroscope({frequency: 60});
+
+
+
+if (window.DeviceOrientationEvent) {
+    window.addEventListener("deviceorientation", function () {
+        tilt([event.beta, event.gamma]);
+    }, true);
+} else if (window.DeviceMotionEvent) {
+    window.addEventListener('devicemotion', function () {
+        tilt([event.acceleration.x * 2, event.acceleration.y * 2]);
+    }, true);
+} else {
+    window.addEventListener("MozOrientation", function () {
+        tilt([orientation.x * 50, orientation.y * 50]);
+    }, true);
+}
+
+function tilt(orientation) {
+
+		  console.log("Angular velocity along the X-axis " + orientation[0]);
+		  console.log("Angular velocity along the Y-axis " + orientation[1]);
+
+
+
+		var widthRatio = (orientation[0] + 180) / 360;
+		colorHue = widthRatio * 360;
+
+		var heightRatio = (orientation[1] + 180) / 360;
+		colorSaturation = heightRatio * 50;
+
+		var coolBackground = document.getElementById("coolBackground");
+
+		coolBackground.style.backgroundColor = "hsl(" + colorHue + ", " + colorSaturation + "%, 70%)";
+
+		// console.log( "hsl(" + colorHue + ", " + colorSaturation + ", 0.7)");
+
+}
+
 
 
 function mousePos (e) {
@@ -17,15 +56,15 @@ function mousePos (e) {
         mouseY = event.clientX + document.body.scrollTop;
 
 		var widthRatio = mouseX / screenWidth;
-		colorHue = widthRatio * 360 * 1.5;
+		colorHue = widthRatio * 360 * 0.4;
 
 		var heightRatio = mouseY / screenHeight;
-		colorSaturation = heightRatio * 50;
+		colorSaturation = heightRatio * 100;
 
 		var coolBackground = document.getElementById("coolBackground");
 
 		coolBackground.style.backgroundColor = "hsl(" + colorHue + ", " + colorSaturation + "%, 70%)";
-		console.log( "hsl(" + colorHue + ", " + colorSaturation + ", 0.7)");
+		// console.log( "hsl(" + colorHue + ", " + colorSaturation + ", 0.7)");
 
 
 		return true;
